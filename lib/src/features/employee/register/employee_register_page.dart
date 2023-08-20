da99be44d6babd328e6378ecc432a07e6788015e
Eduardo Mendonca <ed2aplicativos@gmail.com>
2023-08-20 17:07:59 -0400

    Adicionando VM e State ao Employee Register

--- lib/src/features/employee/register/employee_register_page.dart
+++ lib/src/features/employee/register/employee_register_page.dart
@@ -1,103 +1,159 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:validatorless/validatorless.dart';
+
+import '../../../core/providers/application_providers.dart';
 import '../../../core/ui/widgets/avatar_widget.dart';
+import '../../../core/ui/widgets/barbershop_loader.dart';
 import '../../../core/ui/widgets/hours_panel.dart';
 import '../../../core/ui/widgets/week_days_panel.dart';
+import '../../../model/barbershop_model.dart';
+import 'employee_register_vm.dart';
 
-class EmployeeRegisterPage extends StatefulWidget {
+class EmployeeRegisterPage extends ConsumerStatefulWidget {
   const EmployeeRegisterPage({
     super.key,
   });
 
   @override
-  State<EmployeeRegisterPage> createState() => _EmployeeRegisterPageState();
+  ConsumerState<EmployeeRegisterPage> createState() =>
+      _EmployeeRegisterPageState();
 }
 
-class _EmployeeRegisterPageState extends State<EmployeeRegisterPage> {
+class _EmployeeRegisterPageState extends ConsumerState<EmployeeRegisterPage> {
   var registerADM = false;
+  final formKey = GlobalKey<FormState>();
+  final nameEC = TextEditingController();
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    nameEC.dispose();
+    emailEC.dispose();
+    passwordEC.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final employeeRegisterVM = ref.watch(employeeRegisterVmProvider.notifier);
+    final barbershopAsyncValue = ref.watch(getMyBarbershopProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cadastrar colaborador'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Center(
-            child: Column(
-              children: [
-                const AvatarWidget(),
-                const SizedBox(height: 32),
-                Row(
-                  children: [
-                    Checkbox.adaptive(
-                      value: registerADM,
-                      onChanged: (e) {
-                        setState(() {
-                          registerADM = !registerADM;
-                        });
-                      },
-                    ),
-                    const Expanded(
-                      child: Text(
-                        'Sou administrador e quero me cadastrar como colaborador',
-                        style: TextStyle(
-                          fontSize: 14,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Offstage(
-                  offstage: registerADM,
+      body: barbershopAsyncValue.when(
+        error: (error, stackTrace) {
+          log('Erro ao carregar a página',
+              error: error, stackTrace: stackTrace);
+          return const Center(
+            child: Text('Erro ao carregar apágina'),
+          );
+        },
+        loading: () => const BarbershopLoader(),
+        data: (barbershopModel) {
+          final BarbershopModel(:openingDays, :openingHours) = barbershopModel;
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Form(
+                key: formKey,
+                child: Center(
                   child: Column(
                     children: [
-                      const SizedBox(height: 24),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          label: Text('Nome'),
+                      const AvatarWidget(),
+                      const SizedBox(height: 32),
+                      Row(
+                        children: [
+                          Checkbox.adaptive(
+                            value: registerADM,
+                            onChanged: (e) {
+                              setState(() {
+                                registerADM = !registerADM;
+                                employeeRegisterVM.setRegisterADM(registerADM);
+                              });
+                            },
+                          ),
+                          const Expanded(
+                            child: Text(
+                              'Sou administrador e quero me cadastrar como colaborador',
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Offstage(
+                        offstage: registerADM,
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 24),
+                            TextFormField(
+                              controller: nameEC,
+                              validator:
+                                  Validatorless.required('Nome obrigatório'),
+                              decoration: const InputDecoration(
+                                label: Text('Nome'),
+                              ),
+                            ),
+                            const SizedBox(height: 24),
+                            TextFormField(
+                              controller: emailEC,
+                              validator: Validatorless.multiple([
+                                Validatorless.required('E-mail obrigatório'),
+                                Validatorless.email('E-mail inválido'),
+                              ]),
+                              decoration: const InputDecoration(
+                                label: Text('E-mail'),
+                              ),
+                            ),
+                            const SizedBox(height: 24),
+                            TextFormField(
+                              controller: passwordEC,
+                              validator: Validatorless.multiple([
+                                Validatorless.required('Senha obrigatória'),
+                                Validatorless.min(6,
+                                    'Senha deve conter pelo menos 6 caracteres'),
+                              ]),
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                label: Text('Senha'),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                       const SizedBox(height: 24),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          label: Text('E-mail'),
-                        ),
+                      WeekDaysPanel(
+                        onDayPressed: employeeRegisterVM.addOrRemoveWorkDays,
+                        enabledDays: openingDays,
                       ),
                       const SizedBox(height: 24),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          label: Text('Senha'),
+                      HoursPanel(
+                        startTime: 6,
+                        endTime: 23,
+                        onHourPressed: employeeRegisterVM.addOrRemoveWorkHous,
+                        enabledTimes: openingHours,
+                      ),
+                      const SizedBox(height: 24),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(56),
                         ),
+                        onPressed: () {},
+                        child: const Text('CADASTRAR COLABORADOR'),
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 24),
-                WeekDaysPanel(
-                  onDayPressed: (String day) {},
-                  enabledDays: const ['Seg', 'Qua', 'Sex'],
-                ),
-                const SizedBox(height: 24),
-                HoursPanel(
-                  startTime: 6,
-                  endTime: 23,
-                  onHourPressed: (int hour) {},
-                  enabledTimes: const [6, 7, 8, 9, 16],
-                ),
-                const SizedBox(height: 24),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(56),
-                  ),
-                  onPressed: () {},
-                  child: const Text('CADASTRAR COLABORADOR'),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
---
+++ lib/src/features/employee/register/employee_register_vm.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'employee_register_vm.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$employeeRegisterVmHash() =>
+    r'77f8e55b31e6254c4d56c0ac0af7677c96bf524d';
+
+/// See also [EmployeeRegisterVm].
+@ProviderFor(EmployeeRegisterVm)
+final employeeRegisterVmProvider = AutoDisposeNotifierProvider<
+    EmployeeRegisterVm, EmployeeRegisterState>.internal(
+  EmployeeRegisterVm.new,
+  name: r'employeeRegisterVmProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$employeeRegisterVmHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EmployeeRegisterVm = AutoDisposeNotifier<EmployeeRegisterState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
